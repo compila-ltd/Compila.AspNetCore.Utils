f2ff38798d1a9bcf7bc44bf0ba4fc830fd9e1dee
Eyonys Gonzalez Marcaida <eyonys88@gmail.com>
2026-01-10 15:01:24 -0500

    Add EF Core async paged list extension method
    Introduced EfCorePagedListExtensions with ToPagedListAsync<T> for IQueryable<T>, enabling async paging using EF Core's CountAsync and ToListAsync. Added required using directives for related namespaces.

---
+++ Compila.AspNetCore.Utils/Infrastructure/Extensions/EfCorePagedListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Compila.Net.Utils.Http.RequestsParameters;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Compila.AspNetCore.Utils.Infrastructure.Extensions
+{
+    public static class EfCorePagedListExtensions
+    {
+        public static Task<PagedList<T>> ToPagedListAsync<T>(
+            this IQueryable<T> source,
+            int pageNumber,
+            int pageSize)
+        {
+            return PagedList<T>.ToPagedListAsync(
+                source,
+                pageNumber,
+                pageSize,
+                q => q.CountAsync(),
+                q => q.ToListAsync()
+            );
+        }
+    }
+}
